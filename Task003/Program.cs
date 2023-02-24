23b5a0190569b1c9596961deadcc658a1d1497ec
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-24 22:08:22 +0300

    Решила задачу сама и как на уроке (свое решение в коментариях)

---
+++ Task003/Program.cs
@@ -0,0 +1,66 @@
+﻿Console.WriteLine("Введите число дня недели");
+int numWeek = Convert.ToInt32(Console.ReadLine());
+
+if (numWeek == 1) Console.WriteLine("Понедельник");
+else if (numWeek == 2) Console.WriteLine("Вторник");
+else if (numWeek == 3) Console.WriteLine("Среда");
+else if (numWeek == 4) Console.WriteLine("Четверг");
+else if (numWeek == 5) Console.WriteLine("Пятница");
+else if (numWeek == 6) Console.WriteLine("Суббота");
+else if (numWeek == 7)Console.WriteLine("Воскресенье");
+else
+{
+   Console.WriteLine("Не существует дня недели под этим номером"); 
+}
+
+
+// if (numWeek == 1)
+// {
+//  Console.WriteLine("Понедельник");   
+// }
+// else
+// {
+//     if (numWeek == 2)
+//     {
+//       Console.WriteLine("Вторник");  
+//     }
+//     else
+//     {
+//         if (numWeek == 3)
+//         {
+//           Console.WriteLine("Среда");  
+//         }
+//         else
+//         {
+//             if (numWeek == 4)
+//             {
+//                 Console.WriteLine("Четверг");
+//             }
+//             else
+//             {
+//                 if (numWeek == 5)
+//                 {
+//                     Console.WriteLine("Пятница");
+//                 }
+//                 else
+//                 {
+//                     if (numWeek == 6)
+//                     {
+//                         Console.WriteLine("Суббота");
+//                     }
+//                     else
+//                     {
+//                         if (numWeek == 7)
+//                         {
+//                             Console.WriteLine("Воскресенье");
+//                         }
+//                         else
+//                         {
+//                             Console.WriteLine("Не существует дня недели под этим номером");
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
