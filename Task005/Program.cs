d2f1c24cd66f56dfdc0efdab04b977d7f15a62a3
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-24 23:27:17 +0300

    Добавлено решение 5 задачи

---
+++ Task005/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напиши программу, которая на вход принемает число N
+//а на выходе показывает все целые числа от -N до namespace Name
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+
+if (N > 0)
+{
+    while (count <= N)
+    {
+        Console.Write(count + " ");
+        count++;
+    }
+}
+else
+{
+    Console.WriteLine(" Не верное число ");
+}
