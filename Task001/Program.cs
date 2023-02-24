e8015c0892850a1dd7c8f5a6a33dd802810195f8
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-24 21:17:12 +0300

    Решила задачу 001 с помощью оператора %

--- Task001/Program.cs
+++ Task001/Program.cs
@@ -1,16 +1,35 @@
 ﻿// 0. Демонстрация решения
-
-// 1.
-// 2. выдает его квадрат (число уноженое на само себя).
+//Написать программу 
+// 1.принемает на вход 2 числа
+// 2. проверяет условие что первое это квадрат второго
+// 3. На выходе 2 варианта является, не является
 
 // Например:
-// 4 -> 16
-// -3 -> 9
-// -7 -> 49
+// a =25 b=5 -> Да
+// a =-9 b=3 -> Нет
+// a =5 b=25 -> Нет
 
-Console.WriteLine("Введите число ");
-int number = Convert.ToInt32(Console.ReadLine());
-int square = number * number;
-Console.WriteLine($"Квадрат числа {number} = {square}"); // интерполяция, с помощью $
+Console.WriteLine("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % (numberB*numberB) == 0)
+{
+ Console.WriteLine(numberA + " является квадратом числа " + numberB);  
+}
+
+else
+{
+ Console.WriteLine(numberA + " не является квадратом числа " + numberB);  
+}
+//if(numberA/numberB*numberB == index);
+   //Console.WriteLine(numberA + " является квадратом числа " + numberB);  
+
+//else
+  // Console.WriteLine(a + "не является квадратом числа " + b);
+
+//Console.WriteLine($"Квадрат числа {number} = {square}"); // интерполяция, с помощью $
 //Console.WriteLine("Квадрат числа" + number + "=" + square); // конкатенация, склеивание строк с помощью + и ""
 
