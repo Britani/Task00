755f1b36be937fbd8df0e5f2a5fa8f5d17f96f2f
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-25 22:53:09 +0300

    Исправление ДЗ Task008

--- Task008/Program.cs
+++ Task008/Program.cs
@@ -1,8 +1,8 @@
 ﻿//Напишите программу, которая на вход принемает число N, а на выходе показывает все четные числа от 1 до N
 Console.WriteLine("Чтобы показать все четные числа от 1 до N введите N ");
-int N = Convert.ToInt32(Console.ReadLine());
+int num = Convert.ToInt32(Console.ReadLine());
 int count = 2;
-while (count<N)
+while (count<num)
 {
    Console.Write($"{count}, "); 
    count =count+2;
