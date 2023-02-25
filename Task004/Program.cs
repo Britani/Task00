bdd18333a5e84f77be572b623aa50395e5ba7425
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-25 04:43:34 +0300

    Добавлено решение ДЗ Задача 4

---
+++ Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу которая принемает на вход 3 числа и выдает максимальное из этих чисел.
+Console.WriteLine("Чтобы узнать какое число является максимальным введем первое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь введем второе число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь введем третье число ");
+int numC = Convert.ToInt32(Console.ReadLine());
+int numMax = numA;
+if (numMax <= numB) {
+    numMax=numB;
+ if (numMax <= numC)
+ {
+    numMax=numC;
+    Console.WriteLine($"Максимальным среди этих трех чисел является число {numC}"); 
+ }
+ else Console.WriteLine($"Максимальным среди этих трех чисел является число {numB}");
+}
+else 
+{
+    Console.WriteLine($"Максимальным среди этих трех чисел является число {numA}");
+}
+    
