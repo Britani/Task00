a79505c4a40c77574b5602a6cb99142c2e73b149
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-25 03:12:12 +0300

    Добавлено решение 7 задачи

---
+++ Task007/Program.cs
@@ -0,0 +1,9 @@
+﻿//Написать программу которая принемает на вход трехзначную цифру
+// а на выход показывает последнюю цифру этого числа
+Console.WriteLine("Введите трехзначное число ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int Remainder = num % 10;
+
+    if (num < 1000 && num >= 100) Console.WriteLine($"Последнее число трехзначного числа {num} = {Remainder} ");
+    
+    else Console.WriteLine($"Число {num} не является трехзначным числом");
