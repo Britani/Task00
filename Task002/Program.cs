c5c7a44e7d68ff460827866499ea3685137f9570
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-25 04:10:24 +0300

    Добавлено решение ДЗ Задача 2

---
+++ Task002/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принемает два числа и выдает, какое число меньше, а какое больше.
+
+Console.WriteLine("Чтобы узнать какое число больше, а какое меньше введите первое число ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Теперь введите второе число ");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+    Console.WriteLine($"Большее число из {numA} и {numB} будет число {numA}");
+if (numA < numB)
+    Console.WriteLine($"Большее число из {numA} и {numB} будет число {numB} ");
+else
+    Console.WriteLine("Числа равны");
