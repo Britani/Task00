75de6f8b7fde57a71876e0508c28d9850ab4f7f0
Tatiana Stepanova <kappro12@yandex.ru>
2023-02-25 22:59:17 +0300

    Исправила ДЗ Task 006

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -1,5 +1,5 @@
 ﻿// Напишем программу, которая на вход принемает число и выдает является ли это число четным или нечетным
 Console.WriteLine("Чтобы узнать является ли число четым или нечетным введите число ");
 int num = Convert.ToInt32(Console.ReadLine());
-if (num % 2 == 0) Console.WriteLine($"Число {num} четное ");
-else Console.WriteLine($"Число {num} нечетное ");
+if (num % 2 == 0) Console.WriteLine($"Число {num} является четным ");
+else Console.WriteLine($"Число {num} не является четным");
