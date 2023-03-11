bd92df62865232a9222ede5e2adf2b3509682cc5
Tatiana Stepanova <kappro12@yandex.ru>
2023-03-11 03:30:56 +0300

    Добавлено ДЗ: Task034, Task036, Task038

--- Task031/Program.cs
+++ Task031/Program.cs
@@ -27,13 +27,8 @@ void PrintArray(int[] arr)
     {
         Console.Write($"{arr[i]}, ");
     }
-    Console.Write("]");
+    Console.WriteLine("]");
 }
-
-
-
-
-
 
 int GetSumNegativeElem (int[] arr)
 {
@@ -65,25 +60,3 @@ Console.WriteLine($"Сумма отрицательных элементов = {
 
 
 
-// int[] CreateArrayRndInt(int size, int min, int max)
-// {
-//     int[] arr = new int[size];
-//     Random rnd = new Random();
-
-//     for (int i = 0; i < size; i++)
-//     {
-//         arr[i] = rnd.Next(min, max + 1);
-//     }
-//     return arr;
-// }
-
-// void PrintArray (int[] arr)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
-//         else Console.Write($"{arr[i]}");
-//     }
-//     Console.WriteLine("]");
-// }
