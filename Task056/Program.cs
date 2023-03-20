80280a5580f31d351ffceca860f9e01056f3f781
Tatiana Stepanova <kappro12@yandex.ru>
2023-03-20 23:55:07 +0300

    Добавлено ДЗ после 8 семинара Task054, Task056, Task058

---
+++ Task056/Program.cs
@@ -0,0 +1,91 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] matr = CreateMatrixRndInt(5, 7, -5, 10);
+PrintMatrix(matr);
+Console.WriteLine();
+int[] sum = SumElementsAcrossRows(matr);
+PrintArray(sum);
+int nomberMinSumRows = MinSumRows(sum);
+Console.WriteLine();
+Console.WriteLine($"Строка с минимальной суммой элементов номер {nomberMinSumRows}");
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; //0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] SumElementsAcrossRows(int[,] matrix)
+{
+    int[] sumElementsRows = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumElementsRows[i] += matrix[i, j];
+        }
+    }
+    return sumElementsRows;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[", 3);
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int MinSumRows(int[] array)
+{
+    int min = 0;
+    for (int k = 0; k < array.Length - 1; k++)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] < array[i + 1])  min = i;
+        }
+    }
+
+    return min;
+}
---
+++ Task058/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns]; //0, 1
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+
+// bool СonditionMatrixMultiplication(int[,] matrixD, int[,] matrixF)
+// {
+//     return matrixD.GetLength(1) != matrixF.GetLength(0);
+// }
+
+
+int[,] MultiplicationMatrix(int[,] matrixD, int[,] matrixF)
+{
+    int[,] matrix = new int[matrixD.GetLength(0), matrixF.GetLength(1)];
+    for (int i = 0; i < matrixD.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixF.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrixF.GetLength(0); k++)
+            {
+                matrix[i, j] += matrixD[i, k] * matrixF[k, j];
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] matrixA = CreateMatrixRndInt(3, 2, 2, 5);
+PrintMatrix(matrixA);
+Console.WriteLine();
+int[,] matrixB = CreateMatrixRndInt(3, 4, 2, 5);
+PrintMatrix(matrixB);
+Console.WriteLine();
+if (matrixA.GetLength(1) != matrixB.GetLength(0))
+    Console.WriteLine($"Данные матрицы умножить невозможно ");
+else
+{
+    int[,] matrixC = MultiplicationMatrix(matrixA, matrixB);
+    Console.WriteLine();
+    PrintMatrix(matrixC);
+}
